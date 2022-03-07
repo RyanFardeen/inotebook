65e761a96ffa8d1e9c0218b89290c0652ce1bd51
RyanFardeen <ryanfardeen.a@gmail.com>
2022-03-07 13:22:20 +0530

    For hosting

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
+//RtzAUFcNsy2U20zd
 
-
-const mongoURI = "mongodb+srv://ryan:allahallah001@cluster0.pbjlw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://ryanfardeen:RtzAUFcNsy2U20zd@cluster0.pbjlw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
